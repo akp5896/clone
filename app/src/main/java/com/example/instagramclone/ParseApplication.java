d08cc266ab4e356b91db3ca4f8599518c88240f1
akp5896 <akp5896@psu.edu>
2021-03-07 21:11:28 -0500

    logging in and out implemented

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,22 @@
+package com.example.instagramclone;
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication  extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("uTJg3rpUj5FTCCwAPZQVpLFh2JDHXvI0lcmzrDme")
+                .clientKey("S7GWwck6WDsbewOCdzQeagYrQLOw4FaVxcxCRhsT")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
