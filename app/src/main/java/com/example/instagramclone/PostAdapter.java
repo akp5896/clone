e13320f41b81d88b582d3004c83510de71131c6c
akp5896 <akp5896@psu.edu>
2021-03-07 22:02:45 -0500

    item populated

---
+++ app/src/main/java/com/example/instagramclone/PostAdapter.java
@@ -0,0 +1,72 @@
+package com.example.instagramclone;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.parse.ParseException;
+
+import java.io.File;
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+
+    List<Post> posts;
+    Context context;
+
+    public PostAdapter(Context context, List<Post> posts)
+    {
+        this.context = context;
+        this.posts = posts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        try {
+            holder.bind(posts.get(position));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView tvUsername;
+        ImageView ivCapture;
+        TextView tvPostdescripion;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvUsername = itemView.findViewById(R.id.tvUsername);
+            ivCapture = itemView.findViewById(R.id.ivCapture);
+            tvPostdescripion = itemView.findViewById(R.id.tvPostDescription);
+        }
+
+        public void bind(final Post post) throws ParseException {
+            tvUsername.setText(post.getUser().getUsername());
+            tvPostdescripion.setText(post.getDescription());
+            Bitmap takenImage = BitmapFactory.decodeFile(post.getImage().getFile().getAbsolutePath());
+            ivCapture.setImageBitmap(takenImage);
+        }
+    }
+}
